d3b97d59374b7c55f2028eb4142ceeef489d415a
Mads <mads.overballe@gmail.com>
2019-11-17 18:21:27 +0100

    Firebase authentication

--- app/src/main/java/com/example/leanbudget/MainActivity.java
+++ app/src/main/java/com/example/leanbudget/MainActivity.java
@@ -3,16 +3,22 @@ package com.example.leanbudget;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -23,10 +29,17 @@ import android.view.Menu;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseUser user;
 
     // Floating action button
     private FloatingActionButton fab_main;
@@ -47,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        signIn(null);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            String name = user.getDisplayName().replace("s","$").replace("S","$");
+            Toast.makeText(this, "Welcome " + name, Toast.LENGTH_LONG).show();
+        }
+        else {
+            signIn(null);
+        }
 
         // Floating action button
         fab_main = findViewById(R.id.fab);
@@ -70,6 +93,28 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void signIn(View v) {
+        List<AuthUI.IdpConfig> providers =
+                Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.ic_money)
+                .build();
+
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    public void signOut(View v) {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        signIn(null);
+                    }
+                });
     }
 
     @Override
